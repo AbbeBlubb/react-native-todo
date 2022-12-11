8732a9f4e0e9281ca97277ac5f2120a555d2fd74
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2022-12-11 20:51:58 +0100

    Header

--- App.tsx
+++ App.tsx
@@ -10,19 +10,27 @@ const Input = () => {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <View style={styles.rootContainer}>
       <StatusBar style="auto" />
+
+      <Text style={styles.header}>My ToDo list</Text>
+      <Text>ToDo list</Text>
       <Input />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 20
+  }
 });
