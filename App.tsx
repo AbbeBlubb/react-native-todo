b0d0f118a998918bee16831f61a1a4c6df04211e
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2022-12-12 15:48:48 +0100

    fix Non-supported styling value

--- App.tsx
+++ App.tsx
@@ -50,8 +50,8 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    margin: "30px 20px",
-    padding: 10,
+    marginTop: 40,
+    padding: 20,
     width: "60%",
     borderColor: "#27c09f",
     borderWidth: 2,
