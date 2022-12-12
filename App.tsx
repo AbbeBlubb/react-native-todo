a8f090c5b2a44c463ba778f8506cf9693952bd74
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2022-12-12 18:30:42 +0100

    As a user I want new todos to be added to a list

--- App.tsx
+++ App.tsx
@@ -1,67 +1,66 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { PrimaryButton } from './components/PrimaryButton';
-import { Input } from './components/Input';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { PrimaryButton } from "./components/PrimaryButton";
+import { Input } from "./components/Input";
 
+const TodoList = (props: { todoList: TodoList }) => {
+    return (
+        <View>
+            {props.todoList.map((todo, index) => {
+                return (
+                  <View key={index}>
+                    <Text>{todo}</Text>
 
-
-const ToDoList = () => {
-  return(
-    <View>
-      <Text>map tdDo list here</Text>
-      <Text>ToDo list</Text>
-      <Text>ToDo list</Text>
-      <Text>ToDo list</Text>
-    </View>
-  )
-}
-
+                  </View>
+                );
+            })}
+        </View>
+    );
+};
 
 type Todo = string;
-type TodoList= Array<Todo>
+type TodoList = Array<Todo>;
 
 export default function App() {
-  const [inputText, setInputText] = useState<string>("hej");
-  const [todoList, setTodoList] = useState<TodoList>([]);
+    const [inputText, setInputText] = useState<string>("hej");
+    const [todoList, setTodoList] = useState<TodoList>([]);
 
-  const submitNewTodo = () => {
-    setTodoList([...todoList, inputText])
-    setInputText("");
-    console.log(inputText, todoList)
-  }
+    const submitNewTodo = () => {
+        setTodoList([...todoList, inputText]);
+        setInputText("");
+    };
 
-  return (
-    <View style={styles.rootContainer}>
-      <StatusBar style="auto" />
-      <Text style={styles.header}>My ToDo list</Text>
-      <ToDoList />
-      <Input inputText={inputText} setInputText={setInputText} />
-      <PrimaryButton submitNewTodo={submitNewTodo} />
-    </View>
-  );
+    return (
+        <View style={styles.rootContainer}>
+            <StatusBar style="auto" />
+            <Text style={styles.header}>My ToDo list</Text>
+            <TodoList todoList={todoList} />
+            <Input inputText={inputText} setInputText={setInputText} />
+            <PrimaryButton submitNewTodo={submitNewTodo} />
+        </View>
+    );
 }
 
-
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    rootContainer: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    padding: 20
-  },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+        padding: 20,
+    },
 
-  input: {
-    marginTop: 40,
-    padding: 20,
-    width: "60%",
-    borderColor: "#27c09f",
-    borderWidth: 2,
-  }
+    input: {
+        marginTop: 40,
+        padding: 20,
+        width: "60%",
+        borderColor: "#27c09f",
+        borderWidth: 2,
+    },
 });
