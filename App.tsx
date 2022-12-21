caae30e19d6c56fd7554a5fa70f459534177886d
Alberto F. Chávez Pérez <andalusier@pm.me>
2022-12-21 16:42:01 +0100

    KeyboardAvoidingView and TouchableWithoutFeedback

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { Header } from "./components/Header";
 import { Input } from "./components/Input";
 import { PrimaryButton } from "./components/PrimaryButton";
@@ -30,27 +37,34 @@ export default function App() {
     <View style={styles.rootContainer}>
       <StatusBar style="auto" />
       <Header headerText="My ToDo list" testID="header" />
-      <View style={styles.bodySection}>
-        <TodoList todoList={todoList} deleteTodo={deleteTodo} />
-        <View style={styles.inputSection}>
-          <View style={styles.inputWrapper}>
-            <Input
-              inputText={inputText}
-              setInputText={setInputText}
-              testID="input-new-todo"
-            />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingView}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={styles.touchableWithoutFeedback}>
+          <View style={styles.bodySection}>
+            <TodoList todoList={todoList} deleteTodo={deleteTodo} />
+            <View style={styles.inputSection}>
+              <View style={styles.inputWrapper}>
+                <Input
+                  inputText={inputText}
+                  setInputText={setInputText}
+                  testID="input-new-todo"
+                />
+              </View>
+              <View style={styles.primaryButtonWrapper}>
+                <PrimaryButton
+                  onPress={submitNewTodo}
+                  inputIsValid={inputIsValid}
+                  title="Add todo"
+                  accessibilityLabel="Add a new todo to the list"
+                  testID="button-submit-new-todo"
+                />
+              </View>
+            </View>
           </View>
-          <View style={styles.primaryButtonWrapper}>
-            <PrimaryButton
-              onPress={submitNewTodo}
-              inputIsValid={inputIsValid}
-              title="Add todo"
-              accessibilityLabel="Add a new todo to the list"
-              testID="button-submit-new-todo"
-            />
-          </View>
-        </View>
-      </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -59,8 +73,14 @@ const styles = StyleSheet.create({
   rootContainer: {
     backgroundColor: "#F3F0F1",
   },
-  bodySection: {
+  keyboardAvoidingView: {
     height: "75%", // Header is 25 %
+  },
+  touchableWithoutFeedback: {
+    height: "100%",
+  },
+  bodySection: {
+    height: "100%", 
     justifyContent: "space-between",
   },
   inputSection: {
