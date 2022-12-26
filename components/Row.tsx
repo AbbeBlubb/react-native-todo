f91910405206a4837b47318a8198582bbfeb3bc3
Alberto F. Chávez Pérez <andalusier@pm.me>
2022-12-26 01:04:47 +0100

    Code from List component moved to new Row component

---
+++ components/Row.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SecondaryButton } from "./SecondaryButton";
+import { TTodo } from "../services/types";
+
+interface IRow {
+  item: TTodo;
+  deleteTodo: CallableFunction;
+}
+
+export const Row = ({ item, deleteTodo }: IRow) => {
+  return (
+    <View style={styles.row} key={item.key}>
+      <View style={styles.rowTextContainer}>
+        <Text testID="todo-text" style={styles.rowText}>
+          {item.text}
+        </Text>
+      </View>
+      <View style={styles.rowDeleteContainer}>
+        <SecondaryButton
+          onPress={deleteTodo}
+          callbackArgument={item.key}
+          title="Done!"
+          accessibilityLabel="Remove the todo from the todo list"
+          testID="button-delete-todo"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    marginHorizontal: 20,
+    flexDirection: "row",
+    paddingTop: 20,
+  },
+  rowTextContainer: {
+    flex: 1,
+    fontSize: 30,
+    paddingBottom: 10,
+    borderStyle: "dotted",
+    borderBottomColor: "#DABFB0",
+    borderBottomWidth: 3,
+  },
+  rowText: {
+    fontSize: 23,
+  },
+  rowDeleteContainer: {
+    marginLeft: 20,
+  },
+});
--- layouts/List.tsx
+++ layouts/List.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import { SecondaryButton } from "../components/SecondaryButton";
+import { FlatList } from "react-native";
+import { Row } from "../components/Row";
 import { TTodoList } from "../services/types";
 
 interface IList {
@@ -12,49 +12,7 @@ export const List = ({ todoList, deleteTodo }: IList) => {
   return (
     <FlatList
       data={todoList}
-      renderItem={({ item }) => (
-        <View style={styles.listRow} key={item.key}>
-          <View style={styles.listRowTextContainer}>
-            <Text testID="todo-text" style={styles.listRowText}>
-              {item.text}
-            </Text>
-          </View>
-          <View style={styles.listRowDeleteContainer}>
-            <SecondaryButton
-              onPress={deleteTodo}
-              callbackArgument={item.key}
-              title="Done!"
-              accessibilityLabel="Remove the todo from the todo list"
-              testID="button-delete-todo"
-            />
-          </View>
-        </View>
-      )}
+      renderItem={({ item }) => <Row item={item} deleteTodo={deleteTodo} />}
     />
   );
 };
-
-const styles = StyleSheet.create({
-  list: {
-    width: "100%",
-  },
-  listRow: {
-    marginHorizontal: 20,
-    flexDirection: "row",
-    paddingTop: 20,
-  },
-  listRowTextContainer: {
-    flex: 1,
-    fontSize: 30,
-    paddingBottom: 10,
-    borderStyle: "dotted",
-    borderBottomColor: "#DABFB0",
-    borderBottomWidth: 3,
-  },
-  listRowText: {
-    fontSize: 23,
-  },
-  listRowDeleteContainer: {
-    marginLeft: 20,
-  },
-});
